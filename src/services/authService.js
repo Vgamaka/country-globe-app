8fa717a582a1e9d6afaa5f187cb43a0ebdffcf94
vihan gamaka <it22364388@my.sliit.lk>
2025-04-19 15:23:56 +0530

    Netfliy

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = `${process.env.REACT_APP_API_URL}/auth`;
+const API_URL = process.env.REACT_APP_API_URL + '/auth';
 
 export const registerUser = async (email, password) => {
   return await axios.post(`${API_URL}/register`, { email, password });
